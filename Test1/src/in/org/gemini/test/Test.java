5cf565e9f2c76bdc18c8181fec30a80222b96817
duppalapudiveerraju <duppalapudiveerraju@gmail.com>
2023-12-08 13:25:03 +0530

    Update Test.java

--- Test1/src/in/org/gemini/test/Test.java
+++ Test1/src/in/org/gemini/test/Test.java
@@ -3,6 +3,6 @@ package in.org.gemini.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to java");
+		System.out.println("Welcome to java1");
 	}
 }
